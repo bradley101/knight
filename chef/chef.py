5b5bb3c7bd53e26b072aad67b9fb6c9db08a581f
Shantanu Banerjee <Shantanu.Banerjee@veritas.com>
2020-05-18 04:31:36 +0530

    added submission history code

--- chef/chef.py
+++ chef/chef.py
@@ -17,6 +17,20 @@ rc_file = '.chefrc'
 base_url = 'https://www.codechef.com'
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
 session_limit_url = 'https://www.codechef.com/session/limit'
+lang_codes = {
+    '.cpp': '44',
+    '.c': '11',
+    '.java': '10',
+    '.py': '116',
+    '.cs': '27',
+}
+solution_results = {
+    'runtime error(SIGFPE)': 'Runtime Error',
+    'wrong answer': 'Wrong Answer',
+    'compilation error': 'Compile Error',
+    'time limit exceeded': 'Time Limit Exceeded',
+    'accepted': 'Accepted'
+}
 
 nsi = ns()
 nsi.username = None
@@ -29,6 +43,7 @@ parser = argparse.ArgumentParser(description="CLI Version for Codechef for dummi
 parser.add_argument("-n", "--nologin", help="Perform some actions without logging in", action="store_true")
 parser.add_argument("-l", "--list-contests", help="Lists all the active contests", action="store_true")
 parser.add_argument("-s", "--submit", help="Submit a solution to a problem", nargs=2, metavar=('problem_code', 'solution_location'))
+parser.add_argument("--history", help="List Submission history for a problem", nargs=1, metavar=('problem_code'))
 # parser.add_argument()
 nsi.arg = parser.parse_args()
 nsi.parser = parser
@@ -60,10 +75,14 @@ def parse_arguments():
         exit(0)
     if nsi.arg.list_contests:
         list_active_contests()
-    if nsi.arg.submit:
-        submit(*nsi.arg.submit)
     if not nsi.arg.nologin:
         login()
+    if nsi.arg.history:
+        print_submission_details(*nsi.arg.history)
+    if nsi.arg.submit:
+        submit(*nsi.arg.submit)
+        print_submission_details(nsi.arg.submit[0])
+    
 
 
 def submit(problem_code, solution_file):
@@ -72,12 +91,41 @@ def submit(problem_code, solution_file):
     nsi.browser.open(base_url + '/submit/' + problem_code)
     form = nsi.browser.select_form('form[id="problem-submission"]')
     form.set('files[sourcefile]', solution_file_path)
-    form.set('language', '44')
+    form.set('language', lang_codes[os.path.splitext(solution_file_path)[1]])
     nsi.browser.submit_selected()
     sleep(1)
     nsi.browser.refresh()
-    print (nsi.browser.get_url())
+    print ('Successfully submited')
+
+    pass
+
+
+def print_submission_details(pc):
+    sleep(0.5)
+    nsi.browser.open(base_url + '/status/' + pc + ',' + nsi.username)
+    sub_table = nsi.browser.get_current_page().find('table', class_='dataTable')
+    header = []
+    data = []
+    for th in sub_table.find('thead').find('tr').find_all('th'):
+        header.append(th.text.strip())
+    # header = header[:-1]
+    trs = sub_table.find('tbody').find_all('tr')
+    for tr in trs:
+        _ = []
+        tds = tr.find_all('td')
+        for idx, td in enumerate(tds):
+            if idx == 3:
+                rs_span = td.find('span')
+                if 'title' in rs_span.attrs and rs_span.attrs['title'] != '':
+                    _.append(td.find('span').attrs['title'])
+                else:
+                    _.append(''.join([str(__) for __ in rs_span.find_all(text=True, recursive=False)]))
+            else:
+                _.append(td.text.strip())
+        # _ = _[:-1]
+        data.append(_)
     
+    print(tabulate(data, headers=header, tablefmt='pretty'))
     pass
 
 
@@ -93,7 +141,7 @@ def login():
 
 def is_logged_in():
     tmp = nsi.browser.get_current_page().findAll(text='Logout')
-    print(tmp)
+    # print(tmp)
     if tmp:
         return True
     return False
