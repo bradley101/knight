ebd78da399e1350fabf2c82aefa6f1ff000d0c11
Shantanu Banerjee <Shantanu.Banerjee@veritas.com>
2020-05-21 18:32:58 +0530

    improvements

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup
 from setuptools import find_packages
 from knight.__init__ import __version__ as version
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
 	name = "knight",
 	version = version,
+	long_description = long_description,
+	long_description_content_type="text/markdown",
 	description = 'CLI version of Codechef for dummies...',
 	url = 'https://github.com/bradley101/knight',
 	author = 'Shantanu Banerjee',
